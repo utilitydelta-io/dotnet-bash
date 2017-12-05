ec4e0d1a236088b57b87404d926553518b77ad0b
Tyson Brown <tysonbrown@live.com.au>
2017-12-05 21:32:06 +1000

    standard error and output virtual props

--- UtilityDelta.Bash/Implementation/ProcessWrapper.cs
+++ UtilityDelta.Bash/Implementation/ProcessWrapper.cs
@@ -1,11 +1,13 @@
 ﻿using System.Diagnostics;
+using System.IO;
 
 namespace UtilityDelta.Bash.Implementation
 {
     public class ProcessWrapper
     {
         private readonly Process _process = new Process();
-
+        public virtual StreamReader StandardOutput => _process.StandardOutput;
+        public virtual StreamReader StandardError => _process.StandardError;
         public virtual Process Process => _process;
         public virtual int ExitCode => _process.ExitCode;
         public virtual ProcessStartInfo StartInfo => _process.StartInfo;
